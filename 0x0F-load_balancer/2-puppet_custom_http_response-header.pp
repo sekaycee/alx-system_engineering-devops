645af76a534f92799c8e3c82eee0c86d41ed497e
Kay Cee <se.kaycee@gmail.com>
2023-04-03 18:10:21 +0100

    [Fix] add before prop to order the exec task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -2,20 +2,23 @@
 
 exec {'update':
   provider => shell,
-  command  => 'sudo apt-get -y update'
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install nginx']
 }
 
 exec {'install nginx':
   provider => shell,
-  command  => 'sudo apt-get -y install nginx'
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header']
 }
 
 exec { 'add_header':
   provider    => shell,
-  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf'
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOSTNAME\";/" /etc/nginx/nginx.conf',
+  before   => Exec['restart nginx']
 }
 
 exec { 'restart nginx':
   provider => shell,
-  command  => 'sudo service nginx restart',
+  command  => 'sudo service nginx restart'
 }
