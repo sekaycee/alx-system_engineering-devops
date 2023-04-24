a5ec109fa3ba1c4cf8f2032b7ca14f07585b9d11
Kay Cee <se.kaycee@gmail.com>
2023-04-24 20:10:28 +0100

    feat: add 0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Return to-do list information for a given employee ID '''
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos?userId={}'.format(sys.argv[1])).json()
+    c_tasks = [todo for todo in todos if todo.get('completed') is True]
+
+    print('Employee {} is done with tasks'.format(user.get('name')), end='')
+    print('({}/{}):'.format(len(c_tasks, len(todos)))
+    for task in c_tasks:
+        print('\t {}'.format(task.get('title')))
