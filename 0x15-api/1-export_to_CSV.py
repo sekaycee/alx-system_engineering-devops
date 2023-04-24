3a5c45c0f1d6903c21f201d819b93c94de0ec477
Kay Cee <se.kaycee@gmail.com>
2023-04-24 20:10:50 +0100

    feat: add 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+''' Export to-do list info of a given employee ID to CSV format '''
+import csv
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    userid = sys.argv[1]
+    user = requests.get(url + 'users/{}'.format(userid)).json()
+    name = user.get('username')
+    todos = requests.get(url + 'todos?userId={}'.format(userid)).json()
+    c_tasks = [[userid, name, todo.get('completed'), todo.get('title')]
+               for todo in todos]
+
+    with open('{}.csv'.format(userid), mode='w') as employee_file:
+        for task in c_tasks:
+            csv.writer(employee_file, quoting=csv.QUOTE_ALL).writerow(task)
