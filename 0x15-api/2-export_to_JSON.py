760943f4fb01ac3a94ebd672c3816053bd832b73
Kay Cee <se.kaycee@gmail.com>
2023-04-24 20:11:05 +0100

    feat: add 2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Export to-do list information for a given employee ID to JSON format '''
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/'
+    userid = sys.argv[1]
+    user = requests.get(url + 'users/{}'.format(userid)).json()
+    name = user.get('username')
+    todos = requests.get(url + 'todos?userId={}'.format(userid)).json()
+    c_tasks = ['task': task.get('title'), 'completed': task.get('completed'),
+               'username': name} for todo in todos]
+
+    with open('{}.json'.format(userid), mode='w') as emp_file:
+        json.dump({userid: c_tasks}, emp_file)
